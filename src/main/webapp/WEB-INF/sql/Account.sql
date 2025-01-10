dae5163fec326c0b486cec3e9a8d21061fbc4566
newB <knalala93@gmail.com>
2025-01-10 17:47:16 +0900

    sql 기본테이블 추가

--- src/main/webapp/WEB-INF/sql/Account.sql
+++ src/main/webapp/WEB-INF/sql/Account.sql
@@ -15,3 +15,30 @@ CREATE TABLE Savings (
     interest_rate NUMBER(5, 2) NOT NULL,
     maturity_date DATE NOT NULL
 );
+
+CREATE TABLE Deposit(
+	deposit_id NUMBER PRIMARY KEY,
+	account_id NUMBER,
+	principal_amount NUMBER(15) NOT NULL,
+	interest_rate NUMBER(5, 2) NOT NULL,
+	maturity_date DATE NOT NULL
+);
+
+CREATE TABLE Loan(
+	loan_id	NUMBER PRIMARY KEY, 
+	account_id	NUMBER,
+	principal_amount NUMBER(15, 2)	NOT NULL,
+	interest_rate NUMBER(5, 2)	NOT NULL,
+	repayment_schedule VARCHAR2(500),
+	remaining_balance NUMBER(15, 2)	NOT NULL
+);
+
+CREATE TABLE Card(
+	card_id	NUMBER PRIMARY KEY,
+	user_id NUMBER,
+	card_number VARCHAR2(20) NOT NULL UNIQUE,
+	card_type VARCHAR2(20) NOT NULL,
+	credit_limit NUMBER(15, 2),
+	expiration_date	DATE NOT NULL,
+	created_date DATE
+);
