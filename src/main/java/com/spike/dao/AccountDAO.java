11635c97d5be3f5a200c02597e583d0c1204e521
newB <knalala93@gmail.com>
2025-01-13 10:16:40 +0900

    .

--- src/main/java/com/spike/dao/AccountDAO.java
+++ src/main/java/com/spike/dao/AccountDAO.java
@@ -1,7 +1,11 @@
 package com.spike.dao;
 
+import java.util.List;
+
+import com.spike.dto.AccountDTO;
+
 public interface AccountDAO {
-    void saveAccount(AccountVO account);
-    AccountVO getAccountById(Long accountId);
-    List<AccountVO> getAccountsByUserId(Long userId);
+    void saveAccount(AccountDTO account);
+    AccountDTO getAccountById(Long accountId);
+    List<AccountDTO> getAccountsByUserId(Long userId);
 }
