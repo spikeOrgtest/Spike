a401c6e96280e66d68e7f5e42632fda207a75b87
jungq1023 <hjg1994@naver.com>
2025-01-20 10:21:42 +0900

    AccountTestDTO add

--- src/main/java/com/spike/dto/AccountTestDTO.java
+++ src/main/java/com/spike/dto/AccountTestDTO.java
@@ -2,18 +2,21 @@ package com.spike.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "ACCOUNT")
+@Table(name = "SampleACCOUNT")
 public class AccountTestDTO {
 
     @Id
@@ -33,31 +36,25 @@ public class AccountTestDTO {
     @ManyToOne
     private UserDTO user_id;
 
-    @Column(name = "ACCOUNT_NUMBER", nullable = false, unique = true)
     private String accountNumber;
 
     // 계좌 잔액
-    @Column(nullable = false)
     private BigDecimal balance = BigDecimal.ZERO;
 
     // 계좌 타입 (예: "DEPOSIT", "SAVING", "LOAN", "STOCK", "CRYPTO", etc.)
     // enum 대신 String으로 관리
-    @Column(name = "ACCOUNT_TYPE", nullable = false)
     private String accountType;
 
     // 통화 단위 (기본값 KRW)
-    @Column(name = "CURRENCY", nullable = false)
     private String currency = "KRW";
 
     // 생성 시각
-    @CreatedDate
-    @Column(name = "CREATED_AT", updatable = false)
-    private LocalDateTime createdAt;
+    @CreationTimestamp
+    private Timestamp createdAt;
 
     // 마지막 수정 시각
-    @LastModifiedDate
-    @Column(name = "UPDATED_AT")
-    private LocalDateTime updatedAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
    
 }
