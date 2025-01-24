ef4b2dbf2937033306e6bb9d6cb697a5d8203f11
newB <knalala93@gmail.com>
2025-01-24 15:58:12 +0900

    .

--- src/main/webapp/js/newCard.js
+++ src/main/webapp/js/newCard.js
@@ -73,8 +73,12 @@ document.getElementById('card_name').addEventListener('change', function() {
 
 	if (selectedType === 'SPIKE 카드') {
 		options = [
-			{ value: 'spike_savings', text: 'SPIKE 카드' },
-			{ value: 'housing_savings', text: '이벤트 카드' }
+			{ value: 'YOUTHCARD', text: 'SPIKE 청년 카드' },
+			{ value: 'PLATINUMCARD', text: '플래티넘 카드' },
+			{ value: 'DIGITALCARD', text: '디지털 카드' },
+			{ value: 'SPIKECARD', text: 'SPIKE 카드' },
+			{ value: 'TRAVELCARD', text: 'SPIKE TRAVEL 카드' },
+			{ value: 'PLUSCARD', text: '플러스 카드' }
 		];
 	}
 
