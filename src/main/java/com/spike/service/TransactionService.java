2e89d3e18fcb14133ddf4c2785ba6750ff4e1307
jungkyu Han <jungq94@gmail.com>
2025-02-01 17:29:25 +0900

    2/1 이체 기능 구현(송금페이지, 송금확인페이지(shield포함), 송금내역)

--- src/main/java/com/spike/service/TransactionService.java
+++ src/main/java/com/spike/service/TransactionService.java
@@ -5,16 +5,23 @@ import java.util.List;
 
 import com.spike.dto.AccountDTO;
 import com.spike.dto.TransferDTO;
+import com.spike.dto.TransferHistoryDTO;
 import com.spike.dto.UserDTO;
 
 public interface TransactionService {
 
 	List<AccountDTO> getAccountList(UserDTO user);
 
-	void transfer(Long fromAccountId, String toAccount, long amount, String memo, String accountPassword);
-
 	String getOwnerName(String toAccount);
 
 	void transfer(TransferDTO tData);
+
+	String getOwnerStatus(String toAccount);
+
+	AccountDTO getAccount(Long AccountId);
+
+	UserDTO getOwner(String AccountNumber);
+
+	List<TransferHistoryDTO> getRecentTransfers(Long userId);
 
 }
