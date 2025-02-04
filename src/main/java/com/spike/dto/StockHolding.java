2bc1a1e497c606ac464187f973b0d68b6e4dea62
jungq1023 <hjg1994@naver.com>
2025-02-04 17:14:52 +0900

    StockHolding 안 올라가서 다시 갱신

---
+++ src/main/java/com/spike/dto/StockHolding.java
@@ -0,0 +1,41 @@
+package com.spike.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "StockHolding")
+@SequenceGenerator(
+    name = "Stockholding_seq_generator",
+    sequenceName = "Stockholding_seq",
+    initialValue = 1,
+    allocationSize = 1
+)
+public class StockHolding {
+
+	@Id
+	@GeneratedValue(
+			generator = "Stockholding_seq_generator",
+			strategy = GenerationType.SEQUENCE
+			)
+	private int id;
+	
+	@ManyToOne
+	private UserDTO holder;
+	
+	@ManyToOne
+	private Stock stock;
+	
+	private int quantity;
+	
+}
