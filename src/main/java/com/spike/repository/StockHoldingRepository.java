9a63e101aadd78573e4ba2167055d5e171d5e010
seungjin0920 <seungjin9856@gmail.com>
2025-02-06 19:38:00 +0900

    userdto->securitiesaccountdto 로 엔티티 수정

--- src/main/java/com/spike/repository/StockHoldingRepository.java
+++ src/main/java/com/spike/repository/StockHoldingRepository.java
@@ -1,16 +1,21 @@
 package com.spike.repository;
 
+import com.spike.dto.SecuritiesAccountDTO;
+import com.spike.dto.StockDTO;
 import com.spike.dto.StockHolding;
 import com.spike.dto.UserDTO;
-import com.spike.dto.StockDTO;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StockHoldingRepository extends JpaRepository<StockHolding, Integer> {
 
-    @Query("SELECT COALESCE(SUM(sh.quantity), 0) FROM StockHolding sh WHERE sh.holder = :seller AND sh.stock = :stock")
-    Integer getStockQuantity(@Param("seller") UserDTO seller, @Param("stock") StockDTO stock);
+	@Query("SELECT COALESCE(SUM(sh.quantity), 0) FROM StockHolding sh WHERE sh.holder.accountId = :sellerId AND sh.stock.stockId = :stockId")
+	int getStockQuantity(@Param("sellerId") Long sellerId, @Param("stockId") int stockId);
+
 }
