254f63909b322d5d23a4ea8b6d76aac7d14c6169
star6400 <knalala93@naver.com>
2025-02-07 13:25:52 +0900

    Add method to retrieve active accounts by user

--- src/main/java/com/spike/dao/AccountDAOImpl.java
+++ src/main/java/com/spike/dao/AccountDAOImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spike.dto.AccountDTO;
+import com.spike.dto.UserDTO;
 import com.spike.repository.AccountRepository;
+
 
 @Repository
 public class AccountDAOImpl implements AccountDAO {
@@ -62,5 +64,11 @@ public class AccountDAOImpl implements AccountDAO {
 	public void updateAccount(AccountDTO account) {
 		this.accountrepo.updateAccount(account);
 	}
+
+	@Override
+	public List<AccountDTO> findActiveAccountsByUser(UserDTO user) {
+		return this.accountrepo.findByOwner(user);
+	}
+
 
 }
