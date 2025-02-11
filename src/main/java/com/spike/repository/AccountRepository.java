6efada48533044231143d32e2188e5dedc4169a6
jungq1023 <hjg1994@naver.com>
2025-02-11 17:47:38 +0900

    2/11 로그인시 잔여 일일한도 초기화

--- src/main/java/com/spike/repository/AccountRepository.java
+++ src/main/java/com/spike/repository/AccountRepository.java
@@ -59,4 +59,11 @@ public interface AccountRepository extends JpaRepository<AccountDTO, Long> {
 	@Query("select a from AccountDTO a where a.accountNumber=?1")
 	List<AccountDTO> findbyAccountInfo(String detailValue);
 
+	/*@Modifying
+	@Query("update accountDTO a set a.availableLimit = a.dayLimit where a = ?1")
+	void updateLimit(AccountDTO account);*/
+	@Modifying
+	@Query("update AccountDTO a set a.availableLimit = a.dayLimit where a.owner = ?1")
+	void updateLimit(UserDTO user);
+
 }
--- src/main/java/com/spike/security/UserDetail.java
+++ src/main/java/com/spike/security/UserDetail.java
@@ -1,9 +1,13 @@
 package com.spike.security;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -14,10 +18,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.spike.dto.AccountDTO;
 import com.spike.dto.ManagerDTO;
 import com.spike.dto.UserDTO;
+import com.spike.repository.AccountRepository;
 import com.spike.repository.LoginHistoryRepository;
 import com.spike.repository.UserRepository;
+import com.spike.service.AccountService;
 
 @Service
 public class UserDetail implements org.springframework.security.core.userdetails.UserDetailsService {
@@ -27,6 +34,12 @@ public class UserDetail implements org.springframework.security.core.userdetails
 	
     @Autowired
     private UserRepository userRepo;
+    
+    @Autowired
+    private AccountService accountService;
+    
+    @Autowired //PR전 임시! 서비스로 돌릴 것
+    private AccountRepository accRepo;
 
     private final PasswordEncoder passwordEncoder;
     
@@ -34,12 +47,27 @@ public class UserDetail implements org.springframework.security.core.userdetails
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Transactional //PR전 임시 -> 서비스로 이동할 것
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
         // DB에서 login_id를 이용해 사용자 정보를 가져옵니다.
         UserDTO user = userRepo.findByLoginId(loginId)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다 : " + loginId));
 
+        //일일 한도 초기화
+        if(!user.getLastLogin().toLocalDate().isEqual(LocalDate.now().plusDays(1))) {
+            //1.updateLimit로 처리
+        	
+        	this.accRepo.updateLimit(user);
+        	//2.전체 계좌를 가져와서 세팅 후 save(기존 repository 메서드 재사용)
+        	/*List<AccountDTO> accList = this.accRepo.findByOwner(user);
+        	for(AccountDTO account : accList){
+            	account.setAvailableLimit(account.getDayLimit());
+            }
+            this.accRepo.saveAll(accList);*/
+            
+        }
+        
         // 사용자 정보 저장 (로그인 시 마다)
         user.setLastLogin(LocalDateTime.now());
         userRepo.save(user);  // DB에 저장
@@ -49,6 +77,9 @@ public class UserDetail implements org.springframework.security.core.userdetails
         Llist.setAllTime(user.getLastLogin());
         LoginHis.save(Llist);
 
+        
+        
+        
         // SpikeUser 객체를 반환
         return new SpikeUser(user, user.getLoginId(), user.getPassword(), getAuthority(user));
     }
