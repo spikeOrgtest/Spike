62cd54a6503124b985daf3d96598878a62583c60
seungjin0920 <seungjin9856@gmail.com>
2025-02-12 18:46:24 +0900

    1. 거래내역 페이징, 시간순 정렬
    2.여러 가격의 매물들이 체결되면 최고가가 아니라 최저가 기준으로 현재가가 갱신됨

--- src/main/java/com/spike/service/StockService.java
+++ src/main/java/com/spike/service/StockService.java
@@ -19,6 +19,9 @@ public interface StockService {
 	//거래후 주식의 현재가 갱신 메소드
 	public void updateStockCurrentPrice(StockDTO stock);
 	
+	// 오버로딩: 새로운 가격을 받아 현재가를 갱신하는 메서드
+    public void updateStockCurrentPrice(StockDTO stock, int newPrice);
+	
 	// 주식 이름을 기준으로 검색하는 메서드 추가 (대소문자 무시)
     List<StockDTO> searchStocksByName(String query);
 
--- src/main/java/com/spike/service/StockTransactionService.java
+++ src/main/java/com/spike/service/StockTransactionService.java
@@ -1,10 +1,17 @@
 package com.spike.service;
 
 import com.spike.dto.StockTransaction;
+
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StockTransactionService {
     
 	//증권 계좌 ID에 해당하는 거래 내역을 조회합니다.(구매자 또는 판매자로 참여한 모든 거래 내역)
     List<StockTransaction> getTransactionsByAccountId(Long accountId);
+    
+ //  새로운 페이징 지원 메서드 추가
+    Page<StockTransaction> getTransactionsByAccountId(Long accountId, Pageable pageable);
 }
